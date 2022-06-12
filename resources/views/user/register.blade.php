defaa7c4969c44c3b9c513fa90096a7de90701ac
Damian <shadigan7@gmail.com>
2022-06-12 19:08:48 +0200

    aa

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.home')
 @section('content')
 //<section class="masthead page-section" id="register">
 <div class="container">
--- routes/web.php
+++ routes/web.php
@@ -25,25 +25,26 @@ Route::get('/start', function () {
     return view('index');
 })->name('start');
 
-
-Route::get('/pracownicy', [PracownicyController::class, 'index'])->name('pracownicy.index');
-Route::get('/logindex', [PracownicyController::class, 'logindex'])->name('pracownicy.logindex');
-
-Route::get('/dodaj', [PracownicyController::class, 'dodaj'])->name('pracownicy.dodaj');
-
-Route::get('/edytuj/{id}', [PracownicyController::class, 'edit'])->name('pracownicy.edit');
-Route::put('/zmien/{id}', [PracownicyController::class, 'update'])->name('pracownicy.update');
-
-Route::delete('/usun/{id}', [PracownicyController::class, 'delete'])->name('pracownicy.delete');
-
-Route::post('/zapisz', [PracownicyController::class, 'zapisz'])->name('pracownicy.zapisz');
-
-Route::get('register', [UserController::class, 'register'])->name('register');
-Route::post('register', [UserController::class, 'register_action'])->name('register.action');
-
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-
-Route::get('password', [UserController::class, 'password'])->name('password');
-Route::post('password', [UserController::class, 'password_action'])->name('password.action');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
+Route::group(['middleware'=>"web"], function(){
+    Route::get('/pracownicy', [PracownicyController::class, 'index'])->name('pracownicy.index');
+    Route::get('/logindex', [PracownicyController::class, 'logindex'])->name('pracownicy.logindex');
+    
+    Route::get('/dodaj', [PracownicyController::class, 'dodaj'])->name('pracownicy.dodaj');
+    
+    Route::get('/edytuj/{id}', [PracownicyController::class, 'edit'])->name('pracownicy.edit');
+    Route::put('/zmien/{id}', [PracownicyController::class, 'update'])->name('pracownicy.update');
+    
+    Route::delete('/usun/{id}', [PracownicyController::class, 'delete'])->name('pracownicy.delete');
+    
+    Route::post('/zapisz', [PracownicyController::class, 'zapisz'])->name('pracownicy.zapisz');
+    
+    Route::get('register', [UserController::class, 'register'])->name('register');
+    Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+    
+    Route::get('login', [UserController::class, 'login'])->name('login');
+    Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+    
+    Route::get('password', [UserController::class, 'password'])->name('password');
+    Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+    Route::get('logout', [UserController::class, 'logout'])->name('logout');
+});
